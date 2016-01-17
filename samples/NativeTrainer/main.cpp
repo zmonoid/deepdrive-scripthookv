bfa6c89ba37fff643a821cf09ccd329a6bb352b4
cq <cquiter@gmail.com>
2016-01-16 16:36:05 -0800

    init

---
+++ samples/NativeTrainer/main.cpp
@@ -0,0 +1,33 @@
+/*
+	THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
+				http://dev-c.com			
+			(C) Alexander Blade 2015
+*/
+
+#include "..\..\inc\main.h"
+#include "script.h"
+#include "keyboard.h"
+
+namespace std{
+	class thread;
+}
+
+BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
+{
+	switch (reason)
+	{
+	case DLL_PROCESS_ATTACH:
+		scriptRegister(hInstance, ScriptMain);
+//		scriptRegisterAdditionalThread(hInstance, AlignCameraLoop);
+		scriptRegisterAdditionalThread(hInstance, DetectRoad);
+//		presentCallbackRegister(PresentHooky);
+		keyboardHandlerRegister(OnKeyboardMessage);
+		break;
+	case DLL_PROCESS_DETACH:
+		scriptUnregister(hInstance);
+//		presentCallbackUnregister(PresentHooky);
+		keyboardHandlerUnregister(OnKeyboardMessage);
+		break;
+	}		
+	return TRUE;
+}
